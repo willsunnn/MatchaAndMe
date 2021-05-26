ced10ce31a3f5ffacbfcc500827ebb5104e07dea
Michael Felizardo <mfelizardo@users.noreply.github.com>
2021-05-26 04:08:02 -0700

    refactored code and bugfixed servo timing

--- Embedded/redboard.ino
+++ Embedded/redboard.ino
@@ -24,11 +24,9 @@ float hum;  // var to store hum sensor value
 float temp; // var to store temop sensor value
 
 #define LIGHT_SENSOR A0     // light sensor data pin
-int light_analog;
 float light;                // light level value as %
 
 #define SOIL_SENSOR A1      // soil moisture sensor data pin
-int soil_analog;
 float soil;                 // soil moisture value as %
 
 #define LED D3              // LED pin
@@ -36,7 +34,6 @@ float soil;                 // soil moisture value as %
 #define SERVO D2            // servo pin
 Servo servo;                // create servo object
 int servo_pos;
-int servo_time;
 
 unsigned int nextTime = 0;          // Next time to contact the server
 HttpClient http;
@@ -54,20 +51,19 @@ http_header_t headers[] = {
 http_request_t request;
 http_response_t response;
 
-// Json parser
-JsonParserStatic<256, 20> parser;
-int plant_id = 1;
-bool led_is_on;
-bool valve_is_auto;
-float valve_manual_value;
-float valve_remaining_time;
-float valve_auto_value;
+struct ControlScheme {
+	bool led_is_on;
+	bool valve_is_auto_control;
+	float valve_auto_value;
+	float valve_manual_value;
+	long valve_end_time;
+};
 
-float valve_soil_diff;
-long current_time;
-long valve_time;
-float prev_valve_remaining_time;
-bool valve_closed;
+// Json parser
+int plant_id = 1;
+ControlScheme control;
+
+
 
 
 void setup() {
@@ -87,19 +83,20 @@ void setup() {
     // declare a Particle function to manually control LED
     Particle.function("led", controlLED);
     
-    servo_pos = 0;
-    close_valve();  // initialize water valve to be closed;
-    
     // declare a Particle function to manually control water valve (servo)
     Particle.function("water", controlServo);
     Serial.println("I am done setting up");
     Serial.println();
+
+	// initialize control scheme defaults
+	control.led_is_on = false;
+	control.valve_is_auto_control = false;
+	control.valve_auto_value = 0;
+	control.valve_end_time = millis();
+	control.valve_manual_value = 0;
 }
 
 void loop() {
-    current_time = millis();
-    prev_valve_remaining_time = valve_remaining_time;
-
     // Humidity and Temperature Reading Code //
     
     // Call rht.update() to get new humidity and temperature values from the sensor.
@@ -121,53 +118,26 @@ void loop() {
 	
 	// Light Sensor Reading Code //
 	// low value means dim/little light, high value means bright/lot light
-	light_analog = analogRead(LIGHT_SENSOR);
+	int light_analog = analogRead(LIGHT_SENSOR);
 	light = (light_analog / 4095.0) * 100;
 	
 	// Soil Moisture Reading Code //
 	// low value means there is a lot of moisture, high value means it's dry
-	soil_analog = analogRead(SOIL_SENSOR);
+	int soil_analog = analogRead(SOIL_SENSOR);
 	soil = (1.0 - (soil_analog / 4095.0)) * 100;
-	
-	// Automatic Actuator Code //
     
     // Water Valve (Servo) Control Code
-        // For manual control:
     update_servo();
-    
-    if (valve_remaining_time != prev_valve_remaining_time)
-    {
-        valve_time = millis() + (valve_remaining_time * 1000);
-    }
-    if ((valve_remaining_time) == 0 || (current_time > valve_time))
-    {
-        if (!valve_closed) 
-        { 
-            Serial.println("Closing Valve");
-            close_valve();
-            valve_closed = true;
-        }
-    }
-            // recieve request from web app
-            // adjust servo position based on request
-            // report current state of servo back to web app
-        // For automatic control:
-    
-            // based on soil moisture value, adjust servo position
-            // report current state of servo back to web app
     
     // LED Control Code
     update_led();
-        // For manual control:
-            // recieve request from web app
-            // turn on/off LED based on request
-            // report current state of LED back to web app
-        // For automatic control:
-            // based on light level value, adjust LED if necessary
-            // report current state of LED back to web app
 
 	
 	// Send data to web app
+    sendDataIfTime();
+}
+
+void sendDataIfTime() {
 	if (nextTime > millis()) {
         return;
     }
@@ -187,12 +157,8 @@ void loop() {
     
     // Parse JSON string from response body
     parse_json(response.body);
-    
+
     nextTime = millis() + 10000;
-    
-	
-	//delay(5000);
-	
 }
 
 // Particle Cloud function to manually control the LED
@@ -231,23 +197,15 @@ int controlServo(String command) {
     
 }
 
-void open_valve(float strength)
-{
-    servo.attach(SERVO);
-    servo_pos = strength * 179;
-    servo.write(servo_pos);
-    delay(300);
-    servo.detach();
+int request_control_update() {
+    sprintf(buffer, "/get-control/1");
+    request.path = buffer;
+    
+    get_http_request();
+    parse_json(response.body);
+	return 1;
 }
 
-// helper function that closes the water valve by writing the servo angle to 0
-void close_valve()
-{
-    servo.attach(SERVO);
-    servo.write(0);
-    delay(300);
-    servo.detach();
-}
 
 void get_http_request()
 {
@@ -260,11 +218,14 @@ void get_http_request()
     Serial.println();
 }
 
+
 void parse_json(String json)
 {
+	JsonParserStatic<256, 20> parser;
     parser.clear();
 	parser.addString(json);
-    
+	
+
     // Test if parsing succeeds.
     if (!parser.parse()) {
 		Serial.println("parsing failed");
@@ -273,72 +234,78 @@ void parse_json(String json)
 	
 	// Get Json Values
 	parser.getOuterValueByKey("plant_id", plant_id);
-    parser.getOuterValueByKey("led_is_on", led_is_on);
-    parser.getOuterValueByKey("valve_is_auto", valve_is_auto);
+    parser.getOuterValueByKey("led_is_on", control.led_is_on);
+    parser.getOuterValueByKey("valve_is_auto", control.valve_is_auto_control);
     
     // Serials for debugging
     Serial.printlnf("Plant ID is %d", plant_id);
-    Serial.printlnf("Is LED on: %d", led_is_on);
-    Serial.printlnf("Is valve auto: %d", valve_is_auto);
+    Serial.printlnf("Is LED on: %d", control.led_is_on);
+    Serial.printlnf("Is valve auto: %d", control.valve_is_auto_control);
     
-    if (!valve_is_auto)
+    if (!control.valve_is_auto_control)
     {
-        parser.getOuterValueByKey("valve_manual_value", valve_manual_value);
+		float valve_remaining_time = 0;
+        parser.getOuterValueByKey("valve_manual_value", control.valve_manual_value);
         parser.getOuterValueByKey("valve_remaining_time", valve_remaining_time);
+		control.valve_end_time = millis() + long(valve_remaining_time * 1000);
         
         // Serials for debugging
-        Serial.printlnf("Valve Manual Value: %f", valve_manual_value);
+        Serial.printlnf("Valve Manual Value: %f", control.valve_manual_value);
         Serial.printlnf("Valve Remaining Time: %f", valve_remaining_time);
     }
     else
     {
-        parser.getOuterValueByKey("valve_auto_value", valve_auto_value);
+        parser.getOuterValueByKey("valve_auto_value", control.valve_auto_value);
         
         // Serial for debugging
-        Serial.printlnf("Valve Auto Value: %f", valve_auto_value);
+        Serial.printlnf("Valve Auto Value: %f", control.valve_auto_value);
     }
     Serial.println();
 }
 
-void update_led()
-{
-    if (led_is_on)
-    {
-        digitalWrite(LED, HIGH);
-    }
-    else
-    {
-        digitalWrite(LED, LOW);
-    }
+// For manual control:
+// turn on/off LED based on control scheme from server
+void update_led() {
+	int output = control.led_is_on? HIGH : LOW;
+	digitalWrite(LED, output);
 }
 
-void update_servo()
-{
-    if (valve_is_auto)
-    {
+// For Manual control:
+// 		sets the servo to the value for a specified amount of time
+// For automatic control:
+// 		Sets a target soil moisture and adjusts the servo to meet that soil moisture
+void update_servo() {
+    if (control.valve_is_auto_control) {
         // if current soil moisture is greater than target value, then don't need to open water valve
-        if ((soil / 100) > valve_auto_value) // divided soil by 100 because soil is expressed as %
+        if ((soil / 100.0) > control.valve_auto_value) // divided soil by 100 because soil is expressed as %
         {
-            close_valve();
+            open_valve(0);		// close the valve
         }
         else
         {
-            valve_soil_diff = valve_auto_value - (soil / 100);
+            float valve_soil_diff = control.valve_auto_value - (soil / 100);
             open_valve(valve_soil_diff);    // make valve strength proportional to difference from current soil moisture to desired moisture
-            valve_closed = false;
         }
     }
     else    // manually controlling water valve
     {
-        if (valve_remaining_time > 0)
-        {
-            open_valve(valve_manual_value);
-            current_time = millis();
-            valve_time = millis() + (valve_remaining_time * 1000);
-            valve_closed = false;
-        }
-    }
+		long currTime = millis();
+		float strength = control.valve_manual_value;
+		if(currTime >= control.valve_end_time) {
+			strength = 0;
+		}
+		open_valve(strength);
+	}
 }
 
+void open_valve(float strength) {
+    servo.attach(SERVO);
+    servo_pos = strength * 179;
+    servo.write(servo_pos);
+    delay(300);
+    servo.detach();
+}
 
-
+void close_valve() {
+    open_valve(0);
+}
