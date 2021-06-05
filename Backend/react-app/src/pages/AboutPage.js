45c0eee0d923e8f96a1beed0ddbbfa4e8c3cf159
William Sun <sunwilliam@gmail.com>
2021-06-04 18:43:18 -0700

    Updated About Page to have more info

--- Backend/react-app/src/pages/AboutPage.js
+++ Backend/react-app/src/pages/AboutPage.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import NavBarView from '../components/NavBar';
+import { Typography, Link } from "@material-ui/core";
+import { withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+	bodyDiv: {
+		"color": 'black',
+		"text-align": 'center',
+		"margin": "20px"
+	},
+	sectionDiv: {
+		"color": 'black',
+		"text-align": 'center',
+		"margin": "20px"
+	}
+});
 
 
 class AboutPage extends React.Component {
 	render() {
+		const { classes } = this.props;
 		return (
 		<div>
-			<NavBarView title="About Us"/>
-			<h1>About Page</h1>
+		<NavBarView title="About Us"/>
+		<div className={classes.bodyDiv}>
+
+			<Typography variant="h4" className={classes.title}>
+				About Matcha & Me
+			</Typography>
+			
+			<div className={classes.sectionDiv}>
+			<Typography variant="subtitle1" className={classes.title}>
+				Project Proposal
+			</Typography>
+			<iframe width="420" height="315"
+			src="https://www.youtube.com/embed/GTM4FukWBOQ">
+			</iframe> 
+			</div>
+			
+			<div className={classes.sectionDiv}>
+			<Typography variant="subtitle1" className={classes.title}>
+				Project Demo
+			</Typography>
+			<iframe width="420" height="315"
+			src="https://www.youtube.com/embed/GTM4FukWBOQ">
+			</iframe> 
+			</div>
+
+			<div className={classes.sectionDiv}>
+			<Typography variant="subtitle1" className={classes.title}>
+				Github Link!
+			</Typography>
+			<Typography variant="body1" className={classes.title}>
+				<Link href="https://github.com/willsunnn/MatchaAndMe">
+				https://github.com/willsunnn/MatchaAndMe
+				</Link>
+			</Typography>
+			</div>
+		</div>
 		</div>
 		)
 	}
 }
 
-export default AboutPage;
+export default withStyles(styles, { withTheme: true })(AboutPage);
--- README.md
+++ README.md
@@ -14,3 +14,7 @@
   - used to collect data and relay data to the backend component
   - used to controol plant growth
 
+## Copy of About Us Page:
+```html
+<h4 class="MuiTypography-root MuiTypography-h4">About Matcha &amp; Me</h4><div class="jss2"><h6 class="MuiTypography-root MuiTypography-subtitle1">Project Proposal</h6><iframe src="https://www.youtube.com/embed/GTM4FukWBOQ" width="420" height="315"></iframe></div><div class="jss2"><h6 class="MuiTypography-root MuiTypography-subtitle1">Project Demo</h6><iframe src="https://www.youtube.com/embed/GTM4FukWBOQ" width="420" height="315"></iframe></div><div class="jss2"><h6 class="MuiTypography-root MuiTypography-subtitle1">Github Link!</h6><p class="MuiTypography-root MuiTypography-body1"><a class="MuiTypography-root MuiLink-root MuiLink-underlineHover MuiTypography-colorPrimary" href="https://github.com/willsunnn/MatchaAndMe">https://github.com/willsunnn/MatchaAndMe</a></p></div>
+```
